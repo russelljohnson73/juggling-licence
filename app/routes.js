4fd93b67924a8d81780ae8f2f15886f629f6bb59
“Russell <russell.johnson@bjss.com>
2025-08-05 19:56:57 +0100

    First commit

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,20 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/juggling-balls-answer', function (req, res) {
+
+  // Make a variable and give it the value from 'how-many-balls'
+  var HowManyBalls = req.session.data['HowManyBalls']
+
+  // Check whether the variable matches a condition
+  if (HowManyBalls == "3 or more"){
+    // Send user to next page
+    res.redirect('/juggling-trick')
+  } else {
+    // Send user to ineligible page
+    res.redirect('/ineligible')
+  }
+
+})
